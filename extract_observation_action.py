7359dd9458d0e0bcd07302d0b04ddcc31ba9f0eb
Max <s2105338@ed.ac.uk>
2024-02-04 10:57:08 +0000

    Update how observations are stored

--- extract_observation_action.py
+++ extract_observation_action.py
@@ -2,7 +2,9 @@ from dataclasses import dataclass
 import os
 from typing import Optional, List
 import numpy as np
+import pandas as pd
 import pickle
+from collections import defaultdict
 
 
 @dataclass
@@ -29,7 +31,10 @@ class ExtractObservationAction:
         """
         self._clustered_dataframe = clustered_dataframe
         self._episodes = episodes
-        self._demonstrations = []
+        self._demonstrations = {"observations": [], "actions": []}
+
+        # Create a dataframe with the columns of the demonstrations list
+        self._demonstrations_df = pd.DataFrame(columns=['observations', 'actions'])
 
     def extract_demonstrations(self):
         """Extract observations"""
@@ -48,39 +53,69 @@ class ExtractObservationAction:
                     agent = episode.agents[agent_id]
 
                     # calculate steering angle
-                    theta = self.extract_steering_angle(agent)
+                    delta = self.extract_steering_angle(agent)
+
                     ego_agent_observations = {'time': agent.time, 'vx': agent.vx_vec, 'vy': agent.vy_vec,
-                                              'psi': agent.psi_vec, 'aid': agent_id,
-                                              'eid': episode_id,
+                                              'psi': agent.psi_vec, #'aid': agent_id, TODO: include it!
+                                              #'eid': episode_id,
                                               'distance_left_lane_marking': agent.distance_left_lane_marking,
-                                              'distance_right_lane_marking': agent.distance_right_lane_marking,
-                                              'surroundings': []}
-                    ego_agent_actions = {'ax':  agent.ax_vec, 'ay': agent.ay_vec, 'theta': theta}
+                                              'distance_right_lane_marking': agent.distance_right_lane_marking}
+                    # todo: explain that ego_agent_observations should be a dataframe where the rows are the timestamps
+                    # and the columns are the features. then, we have a list of these dataframes, one for each agent
+
+                    ego_agent_actions = {'ax':  agent.ax_vec, 'ay': agent.ay_vec, 'delta': delta}
 
                     for inx, t in enumerate(agent.time):
-                        surrounding_agents_features = {}
                         # get surrounding agent's information
                         surrounding_agents = agent.object_relation_dict_list[inx]
                         for surrounding_agent_relation, surrounding_agent_id in surrounding_agents.items():
-                            surrounding_agent_feature = SurroundingAgentFeature()
                             if surrounding_agent_id is not None:
                                 surrounding_agent = episode.agents[surrounding_agent_id]
-                                # lat_dist_dict_vec, long_dist_dict_vec are none, so should be recalculated
+                                # todo lat_dist_dict_vec, long_dist_dict_vec are none, so should be recalculated
                                 long_distance, lat_distance = agent.get_lat_and_long(t, surrounding_agent)
                                 surrounding_agent_inx = surrounding_agent.next_index_of_specific_time(t)
-                                surrounding_agent_feature.rel_dx = long_distance
-                                surrounding_agent_feature.rel_dy = lat_distance
-                                surrounding_agent_feature.vx = surrounding_agent.vx_vec[surrounding_agent_inx]
-                                surrounding_agent_feature.vy = surrounding_agent.vy_vec[surrounding_agent_inx]
-                                surrounding_agent_feature.ax = surrounding_agent.ax_vec[surrounding_agent_inx]
-                                surrounding_agent_feature.ay = surrounding_agent.ay_vec[surrounding_agent_inx]
-                                surrounding_agent_feature.psi = surrounding_agent.psi_vec[surrounding_agent_inx]
+                                surrounding_rel_dx = long_distance
+                                surrounding_rel_dy = lat_distance
+                                surrounding_vx = surrounding_agent.vx_vec[surrounding_agent_inx]
+                                surrounding_vy = surrounding_agent.vy_vec[surrounding_agent_inx]
+                                surrounding_ax = surrounding_agent.ax_vec[surrounding_agent_inx]
+                                surrounding_ay = surrounding_agent.ay_vec[surrounding_agent_inx]
+                                surrounding_psi = surrounding_agent.psi_vec[surrounding_agent_inx]
+                            else:
+                                # Set to invalid value if there is no surrounding agent
+                                surrounding_rel_dx = surrounding_rel_dy = surrounding_vx = surrounding_vy = \
+                                    surrounding_ax = surrounding_ay = surrounding_psi = -1 # TODO: set up a value that makes sense for missing values
 
-                            surrounding_agents_features[surrounding_agent_relation] = surrounding_agent_feature
+                            # if the surrounding agent is not in the dictionary, add it
+                            if surrounding_agent_relation not in ego_agent_observations:
+                                ego_agent_observations[f'{surrounding_agent_relation}_rel_dx'] = []
+                                ego_agent_observations[f'{surrounding_agent_relation}_rel_dy'] = []
+                                ego_agent_observations[f'{surrounding_agent_relation}_vx'] = []
+                                ego_agent_observations[f'{surrounding_agent_relation}_vy'] = []
+                                ego_agent_observations[f'{surrounding_agent_relation}_ax'] = []
+                                ego_agent_observations[f'{surrounding_agent_relation}_ay'] = []
+                                ego_agent_observations[f'{surrounding_agent_relation}_psi'] = []
 
-                        ego_agent_observations['surroundings'].append(surrounding_agents_features)
+                            ego_agent_observations[f'{surrounding_agent_relation}_rel_dx'].append(surrounding_rel_dx)
+                            ego_agent_observations[f'{surrounding_agent_relation}_rel_dy'].append(surrounding_rel_dy)
+                            ego_agent_observations[f'{surrounding_agent_relation}_vx'].append(surrounding_vx)
+                            ego_agent_observations[f'{surrounding_agent_relation}_vy'].append(surrounding_vy)
+                            ego_agent_observations[f'{surrounding_agent_relation}_ax'].append(surrounding_ax)
+                            ego_agent_observations[f'{surrounding_agent_relation}_ay'].append(surrounding_ay)
+                            ego_agent_observations[f'{surrounding_agent_relation}_psi'].append(surrounding_psi)
 
-                    self._demonstrations.append((ego_agent_observations, ego_agent_actions))
+                    ego_agent_observations = pd.DataFrame(ego_agent_observations, index=agent.time)
+
+                    # todo: if we want to export the dataset, keep it as is - otherwise only get the values without
+                    # the column names and drop time
+                    ego_agent_observations = ego_agent_observations.drop(columns=['time'])
+                    ego_agent_actions = pd.DataFrame(ego_agent_actions, index=agent.time)
+
+                    ego_agent_observations = ego_agent_observations.values
+                    ego_agent_actions = ego_agent_actions.values
+
+                    self._demonstrations["observations"].append(ego_agent_observations)
+                    self._demonstrations["actions"].append(ego_agent_actions)
 
     @staticmethod
     def extract_steering_angle(agent) -> List:
@@ -116,3 +151,4 @@ class ExtractObservationAction:
             # Saving the data to a file
             with open(episode_path + "/demonstration.pkl", "wb") as file:
                 pickle.dump(self._demonstrations, file)
+

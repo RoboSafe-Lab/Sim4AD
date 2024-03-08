4af760b5d7bab2701c7587a135a885f0a5a7f124
Max <s2105338@ed.ac.uk>
2024-03-08 16:05:22 +0000

    update the bc network by adding deeper linear layer and tanh

--- baselines/bc_baseline.py
+++ baselines/bc_baseline.py
@@ -6,6 +6,7 @@ import pickle
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from shapely import Point
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, Dataset
 from sklearn.model_selection import train_test_split
@@ -14,59 +15,82 @@ from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
 
 from baselines.dataset import AutomatumDataset
+from sim4ad.opendrive import Map, plot_map
 from sim4ad.path_utils import baseline_path
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class LSTM(nn.Module):
-    def __init__(self):
+    def __init__(self, name='bc'):
         super(LSTM, self).__init__()
 
-        # TODO: load other episodes as well!
+        self.name = name
+
         expert_data = {"observations": [], "actions": []}
-        for scenario in ['hw-a9-appershofen-001-d8087340-8287-46b6-9612-869b09e68448', 'hw-a9-appershofen-002-2234a9ae-2de1-4ad4-9f43-65c2be9696d6',
-                         'hw-a9-appershofen-003-6d6e3378-df9b-4130-8cbf-3437a77a309d']:
+        for scenario in [
+                        'hw-a9-appershofen-001-d8087340-8287-46b6-9612-869b09e68448',
+                        'hw-a9-appershofen-002-2234a9ae-2de1-4ad4-9f43-65c2be9696d6',
+                        'hw-a9-appershofen-003-6d6e3378-df9b-4130-8cbf-3437a77a309d',
+                        'hw-a9-appershofen-004-e7ee10c6-a428-4416-bd84-cebb0476f565',
+                        'hw-a9-appershofen-005-dc8c9357-291c-4b27-8c3d-98a048818efd',
+                        'hw-a9-appershofen-006-7e386963-33f2-4e71-a750-76cc66791d43',
+                        'hw-a9-appershofen-007-55244cc7-f80a-49dc-a29d-ed707b6ea4fb',
+                        'hw-a9-appershofen-008-44cb097b-ce86-4d2d-b509-0e0c5b5b7ad5',
+                        'hw-a9-appershofen-009-2caba3d6-ef31-48c8-b8e1-d9c6a300a68a',
+                        'hw-a9-appershofen-011-b932653c-ea9c-424a-a8cc-51fb75ad9d59',
+                        'hw-a9-appershofen-012-d696e4f3-70ac-45ac-9de1-79a2c9f6185c',
+                        'hw-a9-appershofen-013-7e5d812c-a86f-468c-b9ed-d6888991eeb7',
+                        ]:
             with open(f'scenarios/data/trainingdata/{scenario}/demonstration.pkl', 'rb') as f:
                 new_expert_data = pickle.load(f)
                 expert_data['observations'] += new_expert_data['observations']
                 expert_data['actions'] += new_expert_data['actions']
 
-        PADDING_VALUE = -1  # used to pad the LSTM input to the same length
+        self.PADDING_VALUE = -1  # used to pad the LSTM input to the same length
+
         expert_states_all = pad_sequence(
-            [torch.as_tensor(seq, dtype=torch.float32) for seq in expert_data['observations']],
-            batch_first=True,
-            padding_value=PADDING_VALUE)
+                [torch.as_tensor(seq, dtype=torch.float32) for seq in expert_data['observations']],
+                batch_first=True,
+                padding_value=self.PADDING_VALUE)
         expert_actions_all = pad_sequence([torch.as_tensor(seq, dtype=torch.float32) for seq in expert_data['actions']],
                                           batch_first=True,
-                                          padding_value=PADDING_VALUE)
+                                          padding_value=self.PADDING_VALUE)
 
         expert_states_train, expert_states_test, expert_actions_train, expert_actions_test = train_test_split(
             expert_states_all, expert_actions_all, test_size=0.2)
 
         # TODO: should we normalize the data?
-        expert_actions_train = expert_actions_train
-        expert_actions_test = expert_actions_test
-
-        # Discretize the actions to 2 decimal places
-        expert_actions_train = torch.round(expert_actions_train * 100) / 100
-        expert_actions_test = torch.round(expert_actions_test * 100) / 100
 
         input_space = expert_states_train.shape[-1]
         action_space = expert_actions_train.shape[-1]
 
-        self.rnn = nn.LSTM(input_size=input_space, hidden_size=256, num_layers=2, batch_first=True)
-        self.fc = nn.Linear(256, action_space)
-
-        # TODO: should be 34 as we should not include x/y
-        assert input_space == 36 and action_space == 2  # TODO: just for automatum dataset
-        self.loss_function = nn.MSELoss() # TODO nn.MSELoss(reduction="mean")  # TODO: is this the correct loss function / reduction?
-        self.writer = SummaryWriter(f'baselines/runs/bc')
-
         self.BATCH_SIZE = 128  # Define your batch size # TODO: parameterize
         self.SHUFFLE = True  # shuffle your data
-        self.EPOCHS = 100  # Define the number of epochs # TODO: parameterize
-        self.LR = 1e-4  # Define your learning rate # TODO: parameterize
+        self.EPOCHS = 10000  # Define the number of epochs # TODO: parameterize
+        self.LR = 1e-3  # Define your learning rate # TODO: parameterize
+        LSTM_HIDDEN_SIZE = 128
+        FC_HIDDEN_SIZE = 512
+        DROPOUT = 0.2  # TODO: remember to zero it out during evaluation!
+
+        self.rnn = nn.LSTM(input_size=input_space,  hidden_size=LSTM_HIDDEN_SIZE, num_layers=2, batch_first=True)
+        self.fc = nn.Sequential(nn.Linear(LSTM_HIDDEN_SIZE, FC_HIDDEN_SIZE),
+                                nn.ReLU(),
+                                nn.Dropout(DROPOUT),
+                                nn.Linear(FC_HIDDEN_SIZE, action_space),
+                                nn.Dropout(DROPOUT),
+                                nn.Tanh())  # TODO: check the range of acceleration/steering angle in the dataset
+        self.float()
+
+        for layer in self.fc:
+            if isinstance(layer, nn.Linear):
+                nn.init.xavier_normal_(layer.weight)
+                nn.init.constant_(layer.bias, 0.0)
+
+        # TODO: should be 34 as we should not include x/y
+        assert input_space == 34 and action_space == 2  # TODO: just for automatum dataset
+        self.loss_function = nn.MSELoss(reduction="mean") # TODO nn.MSELoss(reduction="mean")  # TODO: is this the correct loss function / reduction?
 
         self.train_loader = DataLoader(AutomatumDataset(expert_states_train, expert_actions_train),
                                        batch_size=self.BATCH_SIZE, shuffle=self.SHUFFLE)
@@ -89,8 +113,17 @@ class LSTM(nn.Module):
         path = baseline_path(baseline_name)
         self.load_state_dict(torch.load(path))
 
+    def compute_loss(self, trajectory, predicted_actions, actions):
+        """ Only compute the loss for the time steps that were not padded """
+
+        mask = ~(trajectory == self.PADDING_VALUE).all(dim=-1)
+        loss = self.loss_function(predicted_actions[mask]*10, actions[mask]*10) # TODO: scaling?
+        return loss
+
     def train(self, num_epochs=100, learning_rate=1e-3):
         best_loss = float('inf')
+
+        self.writer = SummaryWriter(f'baselines/runs/bc')
 
         for epoch in tqdm(range(num_epochs)):
             # Training
@@ -98,18 +131,31 @@ class LSTM(nn.Module):
                 # Divide the trajectory for each time step and feed the LSTM the history up until that point
                 self.optimizer.zero_grad()
                 predicted_actions = self.forward(trajectory)
-                # TODO: 100 is the scale factor
-                loss = self.loss_function(predicted_actions * 10, actions * 10)
+                loss = self.compute_loss(trajectory, predicted_actions, actions)
                 loss.backward()
+
+                # Store the gradients
+                ave_grads = []
+                max_grads = []
+                layers = []
+                for name, param in self.named_parameters():
+                    if param.requires_grad and ("bias" not in name):
+                        layers.append(name)
+                        ave_grads.append(param.grad.abs().mean().item())
+                        max_grads.append(param.grad.abs().max().item())
+                self.writer.add_scalar('Average Gradient', sum(ave_grads) / len(ave_grads), epoch * len(self.train_loader) + i)
+                self.writer.add_scalar('Max Gradient', sum(max_grads) / len(max_grads), epoch * len(self.train_loader) + i)
+
                 self.optimizer.step()
                 self.writer.add_scalar('Train Loss', loss.item(), epoch * len(self.train_loader) + i)
 
             with torch.no_grad():
                 for i, (trajectory, actions) in enumerate(self.eval_loader):
                     output = self.forward(trajectory)
-                    loss = self.loss_function(output, actions)
+                    loss = self.compute_loss(trajectory, output, actions)
                     self.writer.add_scalar('Eval Loss', loss.item(), epoch * len(self.eval_loader) + i)
                     self.eval_losses.append(loss.item())
+                    logger.debug(f"Epoch {epoch}, batch {i}, eval loss {loss.item()}")
                     # Save the model if the validation loss is the best we've seen so far
                     if loss < best_loss:
                         best_loss = loss
@@ -124,12 +170,10 @@ class LSTM(nn.Module):
         
         self.writer.close()
 
-    def save(self, baseline_name='bc'):
-        torch.save(self.state_dict(), baseline_path(baseline_name))
-
-
+    def save(self):
+        torch.save(self.state_dict(), baseline_path(self.name))
 
 
 if __name__ == '__main__':
-    policy_network = LSTM()
+    policy_network = LSTM("bc-all-obs")
     policy_network.train(num_epochs=policy_network.EPOCHS, learning_rate=policy_network.LR)

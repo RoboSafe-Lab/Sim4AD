cced5965f3462d02275b59ef8fc6f6055bc550a7
Max <s2105338@ed.ac.uk>
2024-06-17 12:24:52 +0100

    Update script

--- train_rl_baseline.sh
+++ train_rl_baseline.sh
@@ -2,11 +2,12 @@
 #SBATCH --job-name=bc
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=2
 #SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=10G
+#SBATCH --mem-per-cpu=50G
 #SBATCH --nice
+#SBATCH --gres=gpu:3
 
 export PYTHONPATH=$HOME/Sim4AD/
-
-python ~/Sim4AD/baselines/rl.py
+source venv/bin/activate
+python3 baselines/rl.py

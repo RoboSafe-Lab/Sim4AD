d1fdf8ee8e6e6f582ad9b80475598229b7467472
ChengWang2018 <18771014894@163.com>
2024-07-23 19:35:48 +0100

    update human likeness evaluation

--- evaluation/human_likeness_evaluation.py
+++ evaluation/human_likeness_evaluation.py
@@ -1,47 +1,115 @@
 import gymnasium as gym
 import numpy as np
 from tqdm import tqdm
-
-from simulator.lightweight_simulator import Sim4ADSimulation
-from baselines.sac.model import Actor as SACActor
 import torch
+import os
+import pickle
 
-ep_name = ["hw-a9-appershofen-001-d8087340-8287-46b6-9612-869b09e68448",
-               "hw-a9-appershofen-002-2234a9ae-2de1-4ad4-9f43-65c2be9696d6"]
+from sim4ad.path_utils import get_config_path, get_file_name_trajectories, get_path_offlinerl_model, get_path_sac_model
+from sim4ad.data import ScenarioConfig
+from simulator.lightweight_simulator import Sim4ADSimulation
+from sim4ad.offlinerlenv.td3bc_automatum import TD3_BC_Loader, EvalConfig
+from baselines.sac.model import Actor as SACActor
+from evaluation.evaluation_functions import EvaluationFeaturesExtractor
 
-spawn_method = "dataset_all"
-# "bc-all-obs-5_pi_cluster_Aggressive"  # "bc-all-obs-1.5_pi" "idm"
-policy_type = "sac"  # "follow_dataset"
-clustering = "all"
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class RLEvaluation:
+    """Evaluate offline RL and SAC for human likeness analysis"""
 
-env = gym.make("SimulatorEnv-v0", dataset_split="test")
-model = SACActor(env, device=device).to(device)
-model.load_state_dict(torch.load("best_model_sac_SimulatorEnv-v0__model__1__1721400747.pth"))
+    def __init__(self, actor, episode_names, spawn_method, policy_type, clustering):
+        driving_styles = {
+            "Aggressive": actor,
+            "Normal": actor,
+            "Cautious": actor
+        }
 
-DRIVING_STYLES = {
-    "Aggressive": model,
-    "Normal": model,
-    "Cautious": model
-}
+        self.actor = actor
+        self.env = gym.make("SimulatorEnv-v0", dataset_split="test")
+        self.sim = Sim4ADSimulation(episode_name=episode_names, spawn_method=spawn_method, policy_type=policy_type,
+                                    clustering=clustering, driving_style_policies=driving_styles)
 
-sim = Sim4ADSimulation(episode_name=ep_name, spawn_method=spawn_method, policy_type=policy_type,
-                       clustering=clustering, driving_style_policies=DRIVING_STYLES)
-sim.full_reset()
+    def simulation(self, visualization: bool = False):
+        """Using the simulator to simulate trajectories of all agents"""
+        self.sim.full_reset()
+        # done = False # TODO: uncomment this to run until we use all vehicles
+        # while not done:
+        #     assert spawn_method != "random", "we will never finish!"
+        #     done = sim.step(return_done=True)
 
-# done = False # TODO: uncomment this to run until we use all vehicles
-# while not done:
-#     assert spawn_method != "random", "we will never finish!"
-#     done = sim.step(return_done=True)
+        simulation_length = 50  # seconds
+        for _ in tqdm(range(int(np.floor(simulation_length / self.sim.dt)))):
+            self.sim.step()
 
-simulation_length = 50  # seconds
-for _ in tqdm(range(int(np.floor(simulation_length / sim.dt)))):
-    sim.step()
+        # remove all agents left in the simulation.
+        self.sim.kill_all_agents()
+        if visualization:
+            self.sim.replay_simulation()
 
-# Remove all agents left in the simulation.
-sim.kill_all_agents()
+        simulation_agents = self.sim.evaluator.get_picklable_agents()
 
-sim.replay_simulation()
+        print("Simulation done!")
 
-print("Simulation done!")
+        return simulation_agents
+
+
+def begin_evaluation(simulation_agents, evaluation_episodes):
+    """Evaluate the human likeness using speed, criticality distributions"""
+    evaluator = EvaluationFeaturesExtractor("evaluator", evaluation_episodes)
+    evaluator.load(simulation_agents)
+    evaluator.plot_criticality_distribution()
+    evaluator.plot_speed_distribution()
+    # evaluator.plot_distance_distribution()
+
+
+def main():
+    # configuration
+    policy_type = 'offlinerl'  # "bc-all-obs-5_pi_cluster_Aggressive"  # "bc-all-obs-1.5_pi" "idm"
+    normal_map = "appershofen"
+    spawn_method = "dataset_all"
+    clustering = "all"
+
+    configs = ScenarioConfig.load(get_config_path(normal_map))
+    idx = configs.dataset_split["test"]
+    evaluation_episodes = [x.recording_id for i, x in enumerate(configs.episodes) if i in idx]
+
+    output_dir = get_file_name_trajectories(policy_type, spawn_method, irl_weights=None,
+                                            episode_name=evaluation_episodes, param_config=None)
+
+    # Check if the results are already saved
+    if os.path.exists(output_dir):
+        with open(output_dir, "rb") as f:
+            simulation_agents = pickle.load(f)
+        # Begin the evaluation function
+        begin_evaluation(simulation_agents, evaluation_episodes)
+    else:
+        simulation_agents = None
+        parameter_loader = TD3_BC_Loader(EvalConfig)
+
+        if policy_type == 'offlinerl':
+            model_path = get_path_offlinerl_model()
+            parameter_loader.load_model(model_path)
+            actor = parameter_loader.actor
+            offline_rl_eva = RLEvaluation(actor=actor, episode_names=evaluation_episodes, spawn_method=spawn_method,
+                                          policy_type=policy_type, clustering=clustering)
+            simulation_agents = offline_rl_eva.simulation()
+
+        # TODO: add other policies
+        elif policy_type == 'sac':
+            device = parameter_loader.config.device
+            actor = SACActor(parameter_loader.env, device=device).to(device)
+            actor.load_state_dict(torch.load(get_path_sac_model()))
+            sac_eval = RLEvaluation(actor=actor, episode_names=evaluation_episodes, spawn_method=spawn_method,
+                                    policy_type=policy_type, clustering=clustering)
+            simulation_agents = sac_eval.simulation()
+
+        elif policy_type == 'bc':
+            raise NotImplementedError
+
+        # Create the directory if it does not exist
+        os.makedirs(os.path.dirname(output_dir), exist_ok=True)
+        with open(output_dir, "wb") as f:
+            pickle.dump(simulation_agents, f)
+
+
+if __name__ == "__main__":
+    main()

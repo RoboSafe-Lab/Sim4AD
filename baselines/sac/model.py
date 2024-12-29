79e2e2a8ca0697472c0b1696229be304e3e51795
lxkong1 <klx870958424@163.com>
2024-12-29 21:38:11 +0800

    cautious no td3

--- baselines/sac/model.py
+++ baselines/sac/model.py
@@ -75,7 +75,7 @@ class Args:
     hidden_layer_dim = 256
     """the hidden layer dimension of (all) the networks"""
 
-    cluster: str = "Normal"
+    cluster: str = "Cautious"
     """the clustering method to use. Options include 'All', 'Aggressive', 'Normal', 'Cautious'"""
 
     normalize_state: bool = True
